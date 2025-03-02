e127fb30d36045d6cef8f1517e0a8728da6816f2
ram2005024 <sharmashekhar20050@gmail.com>
2025-03-02 14:24:17 +0545

    React Repo

---
+++ export.js
@@ -0,0 +1,4 @@
+import {sum as sumOfTwoNumbers,multiply as mul} from './index.js'
+console.log(sumOfTwoNumbers(10,20));
+console.log(mul(10,20));
+console.log("Terminated successfully");
---
+++ index.js
@@ -0,0 +1,8 @@
+export let sum=(num1,num2)=>{
+
+return num1+num2;
+}
+export let multiply=(num1,num2)=>{
+
+return num1*num2;
+}
