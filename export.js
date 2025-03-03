756b0032d15924f151d866f2a79576271977041f
ram2005024 <sharmashekhar20050@gmail.com>
2025-03-03 13:56:50 +0545

    Addd Initial

--- export.js
+++
@@ -1,4 +0,0 @@
-import {sum as sumOfTwoNumbers,multiply as mul} from './index.js'
-console.log(sumOfTwoNumbers(10,20));
-console.log(mul(10,20));
-console.log("Terminated successfully");
--- index.js
+++
@@ -1,8 +0,0 @@
-export let sum=(num1,num2)=>{
-
-return num1+num2;
-}
-export let multiply=(num1,num2)=>{
-
-return num1*num2;
-}
